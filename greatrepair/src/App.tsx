d821cffd241ac91d411ce7139ba65c8029e3dcb3
DESKTOP-T097RBK\Arjen <arjentromp12@hotmail.com>
2021-11-02 20:49:00 +0100

    Init

---
+++ greatrepair/src/App.tsx
@@ -0,0 +1,18 @@
+import userEvent from '@testing-library/user-event';
+import React, { ProviderProps } from 'react';
+
+export const Home: React.FC = () => {
+  return <div><div>Hello world</div>
+  <Profile name={'Arjen'} icon={''}/>
+  </div>
+}
+
+type Props = {
+  name: string;
+  icon: string;
+}
+
+export const Profile: React.FC<Props> = (props) => {
+
+  return <>{props.name}</>
+}
